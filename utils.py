62e6203cf347b19d33f8dcf742cb8f1244e6f842
jeff <jeffreyr@securitydept.com>
2024-08-23 15:55:36 -0400

    cleanup

--- utils.py
+++ utils.py
@@ -6,23 +6,23 @@ import torch
 import torch.nn.functional as F
 
 
-from pytorch_lightning.loggers import TestTubeLogger
-
-
-def setup_testube_logger() -> TestTubeLogger:
-    """ Function that sets the TestTubeLogger to be used. """
-    try:
-        job_id = os.environ["SLURM_JOB_ID"]
-    except Exception:
-        job_id = None
-
-    now = datetime.now()
-    dt_string = now.strftime("%d-%m-%Y--%H-%M-%S")
-    return TestTubeLogger(
-        save_dir="experiments/",
-        version=job_id if job_id else dt_string,
-        name="lightning_logs",
-    )
+# from pytorch_lightning.loggers import TestTubeLogger
+#
+#
+# def setup_testube_logger() -> TestTubeLogger:
+#     """ Function that sets the TestTubeLogger to be used. """
+#     try:
+#         job_id = os.environ["SLURM_JOB_ID"]
+#     except Exception:
+#         job_id = None
+#
+#     now = datetime.now()
+#     dt_string = now.strftime("%d-%m-%Y--%H-%M-%S")
+#     return TestTubeLogger(
+#         save_dir="experiments/",
+#         version=job_id if job_id else dt_string,
+#         name="lightning_logs",
+#     )
 
 
 def top_k_top_p_filtering(
